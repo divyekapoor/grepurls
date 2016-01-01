f504dd4bcc45ff53e3f30fdcbc9e8ce922a7bcc0
Divye Kapoor <divyekapoor@gmail.com>
2015-12-31 18:22:59 -0800

    Script to set up gflags.

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Author: divyekapoor@gmail.com (Divye Kapoor)
+#
+# Set up gflags dependencies.
+
+set -e
+
+# Sadly, GFlags requires CMake.
+# Set up GFlags and build the static and dynamic libraries.
+# We will use only the static library though.
+cd gflags
+mkdir build
+cd build
+cmake
+
+# Actually build the library.
+make
+
